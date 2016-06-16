f60868aefce0ee06e099bec12bbb5dbee708c536
Ciprian Turcu <turcuciprian1@gmail.com>
2016-06-16 22:45:18 +0300

    finished base functionality, it works, it's useful

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
     //debug variable
     var debug = true;
     //The main module
-    var pcs = angular.module('slo', ['ui.bootstrap', 'ngAnimate', 'ngStorage']);
+    var slo = angular.module('slo', ['ui.bootstrap', 'ngAnimate', 'ngStorage']);
     // pcs.run(function($rootScope, $templateCache) {
     //     $rootScope.$on('$viewContentLoaded', function() {
     //         $templateCache.removeAll();
@@ -12,27 +12,35 @@
         console.log('Module slo initiated');
     }
 
-    var mainCtrl = pcs.controller('mainCtrl', ['$scope', '$log', '$http', '$localStorage', function($scope, $log, $http, $localStorage) {
+    var mainCtrl = slo.controller('mainCtrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
         if (debug) {
             console.log('inside controller');
         }
         $scope.products = [];
         $scope.total = 0;
-        $scope.addProduct = function(){
-          var newProduct = {
-            name: '',
-            quantity: 1,
-            price: null,
-          };
-          $scope.products.push(newProduct);
+        $scope.addProduct = function() {
+            var newProduct = {
+                name: '',
+                quantity: 1,
+                price: 0,
+            };
+            $scope.products.push(newProduct);
         };
         //calculate total
-        $scope.pcsExport = function(){
-          if(debug){
-            console.log('Export button clicked!');
-          }
+        $scope.calculate = function() {
+          $scope.total = 0;
+          console.log($scope.products);
 
-          var pName = (typeof $scope.pName !== 'undefined' ? $scope.pName : 'Plugin Name');
+            $scope.products.forEach(function(item){
+              $scope.total += (item.quantity*item.price);
+            });
+        };
+        $scope.pcsExport = function() {
+            if (debug) {
+                console.log('Export button clicked!');
+            }
+
+            var pName = (typeof $scope.pName !== 'undefined' ? $scope.pName : 'Plugin Name');
 
 
 
